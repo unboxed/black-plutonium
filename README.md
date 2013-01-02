3d02e1a2e2e81c3c326dff1b182c6d199c9f5f22
Jolyon Pawlyn <jolyon.pawlyn@unboxedconsulting.com>
2013-01-02 19:50:00 +0000

    Initail rails app without active record but with rack-proxy middleware for making calls to the pivotal tracker api.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 black-plutonium
 ===============
+
+Do not use webrick in development as the call to the PivotalTracker api proxy may not work. Instead use puma. ie
+ rails s puma
+
+Any call to /services is routed to https://www.pivotaltracker.com. See config/initailizers/proxy.rb
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlackPlutonium::Application.config.secret_token = 'e13021d65ccc923b011dc8b88ccd832dc212bf1ab03d4818d5a2eb16e269adb3fa9413b46b2e07cbfbf9968f89ef4f3488c76d7267d9fe2cdd47a5a352de5cb1'
