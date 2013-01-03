0c029ae9ba7c93e7cb1532100ba5f624a2886d8b
Carl Whittaker <carl.whittaker@unboxedconsulting.com>
2013-01-03 10:15:01 +0000

    Move files inside rails public...

--- public/lib/request.js
+++ public/lib/request.js
@@ -1,13 +1,12 @@
 define('request', (function () {
   // TODO replace TOKEN and project id
-  // var BASEURL = 'http://www.pivotaltracker.com/services/v4/projects/68108/stories',
-  var BASEURL = 'sample.xml',
-      TOKEN = '';
+  var BASEURL = '/services/v4/projects/68108/stories',
+      TOKEN = '87746d69176b5454a4e2f69bfd69594c';
 
   return function (filter, success, error) {
     var xhr = new XMLHttpRequest();
 
-    xhr.open('GET', BASEURL + '?filter=state:' + filter.join(','), true);
+    xhr.open('GET', BASEURL, true);
     xhr.setRequestHeader('X-TrackerToken', TOKEN);
     xhr.addEventListener('readystatechange', function () {
       if(xhr.readyState === 4) {
